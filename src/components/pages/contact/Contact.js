29677a3047de87e87db6cd06462f264939665b07
palmtopturtle <gieske.jeff@gmail.com>
2018-11-27 19:56:31 -0500

    Added content!

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -7,7 +7,30 @@ const Contact = props => {
     <div>
       <Planet page="contact" title="Contact Me" />
       <div className={styles["content-container"]}>
-        <p>...Under Construction!</p>
+        <p>
+          Thank you for taking the time to look at my website! If you would like
+          to contact me, LinkedIn is probably the easiest way. I always try to
+          respond as quickly as possible.
+        </p>
+
+        <div className={styles["btn-container"]}>
+          <a
+            className={styles["btn-linkedin"]}
+            href="https://www.linkedin.com/in/jeff-g-7b6ba1164/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className={styles["btn-github"]}
+            href="https://github.com/palmtopturtle"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
     </div>
   );
